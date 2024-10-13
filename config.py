02c53c7e1add8ace0a36c046e9823287b69ae0c7
Ishu_official <somnathhor12052005@gmail.com>
2024-10-14 00:35:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/MISH0009/ShivXMusic",
+    "https://github.com/tutorialwindoxIk/Fast_v2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
